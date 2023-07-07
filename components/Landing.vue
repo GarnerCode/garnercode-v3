a48d92033fc134a905ec44a3e97167258e667d63
Tyler Garner <tyler@garnercode.io>
2023-07-07 10:26:53 -0500

    Styling landing component

--- components/Landing.vue
+++ components/Landing.vue
@@ -6,18 +6,25 @@
             <p>I bring ideas to life on the web.</p>
         </div>
         <button class="button button-primary">Contact Me</button>
+        <div class="headshot-border headshot-border-top"></div>
+        <div class="headshot-border headshot-border-left"></div>
+        <div class="headshot-container">
+            <img src="images/headshot.webp" alt="Tyler Garner headshot">
+        </div>
     </section>
 </template>
 
 <style lang="scss">
     @media screen and (min-width: 0px) {
         #landing {
-            background-color: #3170cf;
+            background-color: #3959DC;
             padding: 0.75rem;
             margin: 1rem;
             display: flex;
             flex-direction: column;
+            justify-content: space-between;
             gap: 3rem;
+            position: relative;
             h1 {
                 color: #98B8E7;
                 .highlight {
@@ -26,6 +33,39 @@
             }
             p {
                 color: white;
+            }
+            .headshot-container {
+                width: 100px;
+                height: 100px;
+                background-color: var(--color-background);
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                bottom: 0;
+                right: 0;
+                border-top-left-radius: 50px;
+            }
+            .headshot-border {
+                position: absolute;
+                width: 25px;
+                height: 25px;
+                border-bottom-right-radius: 28px;
+                box-shadow: 10px 10px 0 10px var(--color-background);
+                background-color: #3959DC;
+            }
+            .headshot-border-top {
+                bottom: 100px;
+                right: 0;
+            }
+            .headshot-border-left {
+                bottom: 0;
+                right: 100px;
+            }
+            img {
+                width: 75px;
+                border-radius: 100%;
             }
         }
     }
